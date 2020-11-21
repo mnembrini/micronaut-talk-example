930ef7155f24391f780f6f257b5dc9e12063b780
Marco Nembrini <marco@3ap.ch>
2020-11-21 18:49:17 +0100

    Add remoting from Micronaut to Spring

---
+++ vaccination/src/main/java/ch/aaap/talks/micronaut/RemotingOutbound.java
@@ -0,0 +1,26 @@
+package ch.aaap.talks.micronaut;
+
+import io.micronaut.context.annotation.Bean;
+import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Value;
+import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
+import org.springframework.remoting.httpinvoker.SimpleHttpInvokerRequestExecutor;
+
+@SuppressWarnings("deprecation")
+@Factory
+public class RemotingOutbound {
+
+    @Value("${language.service.remoting.base.url:http://localhost:50002}")
+    private String languageServiceRemotingBase;
+
+    @Bean
+    public LanguageService createVaccinationServiceInvoker() {
+        HttpInvokerProxyFactoryBean invoker = new HttpInvokerProxyFactoryBean();
+        invoker.setHttpInvokerRequestExecutor(new SimpleHttpInvokerRequestExecutor());
+        invoker.setServiceUrl(languageServiceRemotingBase + LanguageService.URI);
+        invoker.setServiceInterface(LanguageService.class);
+        invoker.afterPropertiesSet();
+        return (LanguageService) invoker.getObject();
+    }
+
+}
--- vaccination/src/main/java/ch/aaap/talks/micronaut/VaccinationServiceImpl.java
+++ vaccination/src/main/java/ch/aaap/talks/micronaut/VaccinationServiceImpl.java
@@ -1,6 +1,7 @@
 package ch.aaap.talks.micronaut;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 import javax.inject.Singleton;
 import org.slf4j.Logger;
@@ -11,9 +12,20 @@ public class VaccinationServiceImpl implements VaccinationService {
 
     private static final Logger log = LoggerFactory.getLogger(VaccinationServiceImpl.class);
 
+    private final LanguageService languageService;
+
+    public VaccinationServiceImpl(LanguageService languageService) {
+        this.languageService = languageService;
+    }
+
     @Override
     public List<Vaccine> findVaccinesFor(PetType petType) {
         log.info("FindVaccinesFor {}", petType);
+
+        log.info("Loading current language");
+        Locale currentLanguage = languageService.getCurrentLanguage();
+        log.info("Current language is {}", currentLanguage);
+
         Vaccine catVaccine = new Vaccine("cat vaccine", PetType.CAT, 60);
         Vaccine earlyCatVaccine = new Vaccine("early cat vaccine", PetType.CAT, 10);
         Vaccine dogVaccine = new Vaccine("dog vaccine", PetType.DOG, 100);
